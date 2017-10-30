296188a7122a8e374f0e495d725d4e2df38d0cf6
Didrik Sæther <didrik.sather@gmail.com>
2017-10-30 18:34:39 +0100

    Changes to all files for accuratly reflecting jsf-style of images. Altso major rework to error-pages.

--- DAT250-auction/target/generated-sources/annotations/entities/Seller_.java
+++ DAT250-auction/target/generated-sources/annotations/entities/Seller_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-27T16:13:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-30T17:56:36")
 @StaticMetamodel(Seller.class)
 public class Seller_ { 
 
